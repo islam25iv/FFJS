77fadf151069101762977e552d123caf8f5995ba
islam abdalroof <38134852+islam25iv@users.noreply.github.com>
2023-10-31 20:13:27 +0200

    Update @ outo v2 .user.js

--- @ outo v2 .user.js
+++ @ outo v2 .user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         auto - Lavander v5
+// @name         auto - Lavander v5.2
 // @namespace    https://fb.me/islam25iv
-// @version      5.0
+// @version      5.2
 // @description  plant-harvest-40%savetime-automatic + collect_product + refill_object
 // @author       islam25iv
 // @match        *://farm-us.centurygames.com/*
